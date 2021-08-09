270578c59204034ebf7c8614ed01bd3b6dae5e4d
Danito90 <daniel_14_559@hotmail.com>
2021-08-08 23:22:26 -0300

    Verificacion cheques unicos
    verifique que en conjunto el numero de cheque con el banco sea unico. Desaparece el cartel de cargad ocorrectamente.

--- AxionApp/chPropios/models.py
+++ AxionApp/chPropios/models.py
@@ -10,7 +10,7 @@ from proveedor.models import Proveedor
 class chPropio(models.Model):
     proveedor = models.ForeignKey(Proveedor, on_delete=models.SET_NULL, null=True)
     banco = models.CharField(max_length=50, choices=BANCOS)
-    numCh = models.CharField(max_length=8, unique=True)
+    numCh = models.CharField(max_length=8,)
     fechaCarga = models.DateField(default=datetime.now) # (auto_now_add=True) para que no se muestre el campo
     fechaVto = models.DateField(auto_now=False)
     monto = models.DecimalField(max_digits=15, decimal_places=2)
@@ -20,6 +20,10 @@ class chPropio(models.Model):
         ordering = ["monto"]
         verbose_name = "Cheque Propio"
         verbose_name_plural = "Cheques Propios"
+        #unique_together = (('banco', 'numCh',),) # par de campos unicos en tupla
+        constraints = [
+            models.UniqueConstraint(fields=['banco', 'numCh',], name='Cheque unico')
+        ] # tambien para campos unicos en pares
 
     def __str__(self):
         return f"Persona {self.id}: Proveedor: {self.proveedor}, Cheque: {self.banco}-{self.numCh}, Vencimiento: {self.fechaVto}, Monto: ${self.monto}"
